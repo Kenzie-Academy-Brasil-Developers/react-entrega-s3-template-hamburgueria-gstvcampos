555d2963bb714e7e5678a46a8461014a0992a955
gstvcampos <gustavo.bmc76@gmail.com>
2023-06-09 05:41:48 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from 'react'
-import { api } from './services/api'
+import Feed from "./pages/Feed"
+
 
 function App() {
-  const [listBurguers, setlistBurguers] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const getBurguers = async () => {
-      try {
-        const reponse = await api.get('/products')    
-
-        setlistBurguers(reponse.data)
-      } catch (error) {
-
-        //criar toast erro
-        console.error(error)
-
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    getBurguers()
-
-  }, [])
-
-
-  if(isLoading) {
-    return <div>Loagind...</div>
-  }
-  // ou colocar em condicional isLoadign ? carregamento : pagina
 
   return (
     <>
-      <p>salve</p>
-    
-    
+      <Feed/>
     </>
       
   )
---
+++ src/pages/Feed/index.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
+function Feed() {
+  const [listBurguers, setListBurguers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const getBurguers = async () => {
+      try {
+        const reponse = await api.get('/products')
+
+        setListBurguers(reponse.data)
+      } catch (error) {
+
+        //criar toast erro
+        console.error(error)
+
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    getBurguers()
+
+  }, [])
+
+
+  //criar toast
+  if (isLoading) {
+    return <div>Loagind...</div>
+  }
+  // ou colocar em condicional isLoadign ? carregamento : pagina
+
+  return (
+    <>
+      <p>salve</p>
+
+
+    </>
+
+  )
+}
+
+export default Feed
